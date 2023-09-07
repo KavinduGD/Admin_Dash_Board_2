54f0d43b73a8610ffdc1b157595b58dc48efe541
KavinduGD <it21038914@my.sliit.lk>
2023-09-07 16:20:04 +0530

    1.30

--- react-admin-ui/src/pages/home/Home.tsx
+++ react-admin-ui/src/pages/home/Home.tsx
@@ -1,10 +1,15 @@
+import BarChartBox from "../../components/barChartBox/BarChartBox";
+import BigChartBox from "../../components/bigChartBox/BigChartBox";
 import ChartBox from "../../components/chartBox/ChartBox";
+import PieChartBox from "../../components/pieChartBox/PieChartBox";
 import TopBox from "../../components/topBox/TopBox";
 import {
   chartBoxUser,
   chartBoxRevenue,
   chartBoxProduct,
   chartBoxConversion,
+  barChartBoxVisit,
+  barChartBoxRevenue,
 } from "../../data/data";
 import "./home.scss";
 function Home() {
@@ -19,16 +24,24 @@ function Home() {
       <div className="box box3">
         <ChartBox {...chartBoxProduct} />
       </div>
-      <div className="box box4">s</div>
+      <div className="box box4">
+        <PieChartBox />
+      </div>
       <div className="box box5">
         <ChartBox {...chartBoxRevenue} />
       </div>
       <div className="box box6">
         <ChartBox {...chartBoxConversion} />
       </div>
-      <div className="box box7">Box7</div>
-      <div className="box box8">Box8</div>
-      <div className="box box9">Box9</div>
+      <div className="box box7">
+        <BigChartBox />
+      </div>
+      <div className="box box8">
+        <BarChartBox {...barChartBoxVisit} />
+      </div>
+      <div className="box box9">
+        <BarChartBox {...barChartBoxRevenue} />
+      </div>
     </div>
   );
 }
